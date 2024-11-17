8801e37741b35cec0ff7c1170eabd710a0eac442
Giovax0Fail <g.ferrante5@outlook.com>
2024-11-17 17:37:25 +0100

    ADD

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {MatInputModule} from '@angular/material/input';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'corso_angular';
+
+    oninput(e:Event)
+    {
+      this.title = (<HTMLInputElement>e.target).value;
+  console.log((<HTMLInputElement>e.target).value);
+  }
+
+ persone = [
+    { nome: 'Davide', cognome: 'Cangiano', isOnline: true },
+    { nome: 'Leonardo', cognome: 'Firenze', isOnline: false },
+    { nome: 'Christian', cognome: 'Damiano', isOnline: true },
+    { nome: 'Marco', cognome: 'Scognamiglio', isOnline: false },
+    { nome: 'Pasquale', cognome: 'De Micco', isOnline: true },
+    { nome: 'Gabriele', cognome: 'Intoccia', isOnline: false },
+    { nome: 'Alessandro', cognome: 'Allegro', isOnline: true }
+  ];
+
+ onclick(event:Event)
+ {
+this.title="Ho cliccato sul bottone";
+ }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
+import {MatInputModule} from '@angular/material/input';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProvaComponent } from './prova/prova.component';
@@ -9,6 +9,7 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import {MatCardModule} from '@angular/material/card';
 import {MatChipsModule} from '@angular/material/chips';
 import {MatButtonModule} from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
@@ -20,7 +21,9 @@ import {MatButtonModule} from '@angular/material/button';
     MatSlideToggleModule,
     MatCardModule,
     MatChipsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatInputModule,
+    FormsModule,
   ],
   providers: [
     provideClientHydration(),
